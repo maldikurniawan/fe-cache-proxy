760acc8c896ce0c695149eb6a84ab4a6afc6cb35
maldikurniawan <akurniawan2102@gmail.com>
2024-04-16 16:10:12 +0700

    add login&register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-white h-[100vh] flex justify-center items-center bg-cover" style={{ "background": "url('../src/assets/bg.jpg')" }}>
+      <Routes>
+        <Route path="" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </div>
   )
 }
