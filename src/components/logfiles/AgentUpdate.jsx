3a3c0a5fe8c04d036a680df92ba8a9d949a356d7
maldikurniawan <akurniawan2102@gmail.com>
2024-10-16 12:59:11 +0700

    fix socket

--- src/components/logfiles/AgentUpdate.jsx
+++ src/components/logfiles/AgentUpdate.jsx
@@ -1,7 +1,26 @@
 // src/components/AgentUpdate.jsx
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const AgentUpdate = () => {
+    const [logData, setLogData] = useState([]);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://127.0.0.1:8000/ws/update-user-agent-log/');
+        socket.onopen = () => {
+            console.log('WebSocket connection established');
+        };
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            // Update logData state with received data
+            setLogData(prevData => [...prevData, data]);
+            socket.onclose = (event) => {
+                console.log('WebSocket connection closed:', event);
+            };
+            return () => {
+                socket.close();
+            };
+        };
+    }, []);
 
     useEffect(() => {
         const fetchAndUpdateCache = async () => {
--- src/components/logfiles/CacheUpdate.jsx
+++ src/components/logfiles/CacheUpdate.jsx
@@ -1,7 +1,26 @@
 // src/components/CacheUpdate.jsx
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const CacheUpdate = () => {
+    const [logData, setLogData] = useState([]);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://127.0.0.1:8000/ws/update-cache-log/');
+        socket.onopen = () => {
+            console.log('WebSocket connection established');
+        };
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            // Update logData state with received data
+            setLogData(prevData => [...prevData, data]);
+            socket.onclose = (event) => {
+                console.log('WebSocket connection closed:', event);
+            };
+            return () => {
+                socket.close();
+            };
+        };
+    }, []);
 
     useEffect(() => {
         const fetchAndUpdateCache = async () => {
