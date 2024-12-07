5b90191db353f9401ba837d1580cb01eeb611932
maldikurniawan <akurniawan2102@gmail.com>
2024-12-07 23:19:52 +0700

    fix route

--- src/pages/Login/PrivateRoute.jsx
+++ src/pages/Login/PrivateRoute.jsx
@@ -3,12 +3,12 @@ import { Navigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
-    // const jwt_access = localStorage.getItem("jwt_access");
-    // if (jwt_access) {
+    const jwt_access = localStorage.getItem("jwt_access");
+    if (jwt_access) {
         return <Outlet />;
-    // } else {
-    //     return <Navigate to="/login" replace />;
-    // }
+    } else {
+        return <Navigate to="/login" replace />;
+    }
 };
 
 export default PrivateRoute;
